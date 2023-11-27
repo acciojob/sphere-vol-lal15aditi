e5017883c34fb329231c605896e8287b5e028f21
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-27 23:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
     //Write your code here
-  
+  const rad = document.getElementById('radius').value;
+
+  // Calculate the volume of the sphere
+  const vol = (4 / 3) * Math.PI * Math.pow(rad, 3);
+
+  // Display the volume in the volume field
+  document.getElementById('volume').value = vol.toFixed(2); // Adjust to the desired decimal places
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = function() {
+    volume_sphere();
+    return false; // To prevent form submission and page refresh
+  };
+};
